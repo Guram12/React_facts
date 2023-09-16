cf3ac139c2868146399f495702092965a7cc39c8
Guram12 <guram.shanidze.33@gmail.com>
2023-09-16 22:43:41 +0400

    added navbar section

---
+++ src/header.js
@@ -0,0 +1,19 @@
+import "./Header.css"
+
+export function Header() {
+    return (
+        <header>
+            <div className="container" >
+                    <div className="header">
+                        <div className="logo_facts">
+                            <img src="./logo192.png" alt="React Logo"/>
+                            <h1>ReacrFacts</h1>
+                        </div>
+                        <div className="react_course">React Course - Project 1</div>
+                    </div>
+            </div>
+        </header>
+    )
+}
+
+
